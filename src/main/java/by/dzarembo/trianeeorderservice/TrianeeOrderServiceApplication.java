dfe11d55cc4625a13f1a7e82ec1d2e41d7b8945d
dzarembo <dima.zarembo@gmail.com>
2026-04-01 13:46:21 +0300

    Init commit

---
+++ src/main/java/by/dzarembo/trianeeorderservice/TrianeeOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package by.dzarembo.trianeeorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrianeeOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrianeeOrderServiceApplication.class, args);
+	}
+
+}
